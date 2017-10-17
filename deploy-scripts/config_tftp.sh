cb54317f26d4ca07420d6160110a6beeae8bfeb3
qinsl0106 <qinsl0106@thundersoft.com>
2017-10-17 19:29:57 +0800

    install centos

--- deploy-scripts/config_tftp.sh
+++ deploy-scripts/config_tftp.sh
@@ -7,12 +7,15 @@ TFTP_LINARO_GRUB=linaro_install/grub.cfg
 
 function config_tftp() {
     local tree_name="$1"
+    local distro_name=${2:-"Centos"}
     # config uefi
     if [ "${tree_name}" = 'linaro' ];then
         cp -f /tftp/linaro_install/CentOS/linaro_centos.grub.cfg /tftp/linaro_install/grub.cfg
     elif [ "${tree_name}" = 'open-estuary' ];then
         cp -f /tftp/estuary_install/grub.cfg /tftp/grub.cfg
     fi
+
+    cp -f /tftp/estuary_install/estuary.txt.${distro_name,,} /tftp/estuary_install/estuary.txt
 }
 
 config_tftp "$@"
