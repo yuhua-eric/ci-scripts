8d3259013bd59c5747d19595b7523ab4a51efd2e
qinshulei <qinsl0106@thundersoft.com>
2017-11-04 21:44:40 +0800

    add dhcp device

--- scripts/check_all_device_power_status.sh
+++ scripts/check_all_device_power_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device_list=$(grep -iRh "^  ip: " configs/ | cut -c 7-)
+device_list=$(grep -iRh "^  bmc: " configs/ | cut -c 7-)
 for device in ${device_list};do
     echo "device : ${device}"
     ipmitool -H "${device}" -I lanplus -U root -P Huawei12#$ power status
