03be9a31e4e59d1dc704315d112f1e293b29c574
qinshulei <qinsl0106@thundersoft.com>
2017-11-04 21:42:44 +0800

    update

--- scripts/check_all_device_power_status.sh
+++ scripts/check_all_device_power_status.sh
@@ -2,5 +2,6 @@
 
 device_list=$(grep -iRh "^  ip: " configs/ | cut -c 7-)
 for device in ${device_list};do
+    echo "device : ${device}"
     ipmitool -H "${device}" -I lanplus -U root -P Huawei12#$ power status
 done
