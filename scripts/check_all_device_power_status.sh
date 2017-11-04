e5d5a8ff2a698c685170d1d5e3893a3e3ef5d3c4
qinshulei <qinsl0106@thundersoft.com>
2017-11-04 22:11:11 +0800

    check all mac address

--- scripts/check_all_device_power_status.sh
+++ scripts/check_all_device_power_status.sh
@@ -4,4 +4,5 @@ device_list=$(grep -iRh "^  bmc: " configs/ | cut -c 7-)
 for device in ${device_list};do
     echo "device : ${device}"
     ipmitool -H "${device}" -I lanplus -U root -P Huawei12#$ power status
+    ipmitool -H "${device}" -I lanplus -U root -P Huawei12#$ lan print | grep 'MAC Address'
 done
