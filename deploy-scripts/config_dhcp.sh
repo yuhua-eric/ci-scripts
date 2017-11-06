1c9cd35e4a01ce49f915904a7f058289bcee7393
qinshulei <qinsl0106@thundersoft.com>
2017-11-06 23:03:05 +0800

    update

--- deploy-scripts/config_dhcp.sh
+++ deploy-scripts/config_dhcp.sh
@@ -15,19 +15,15 @@ DHCP_FILENAME=dhcpd.conf
 
 function config_dhcp() {
     local tree_name="$1"
-    # config dhcp
-    if [[ "${CI_ENV}" == "test" ]];then
-        cd ..; ./scripts/gen_dhcpd_conf.sh > /etc/dhcp/dhcpd.conf;cd -
-        service isc-dhcp-server restart
-    else
-        if [ "${tree_name}" = 'linaro' ];then
-            sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} cp -f /etc/dhcp/examples/dhcpd.conf.linaro /etc/dhcp/dhcpd.conf
-        elif [ "${tree_name}" = 'open-estuary' ];then
-            sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} cp -f /etc/dhcp/examples/dhcpd.conf.estuary /etc/dhcp/dhcpd.conf
-        fi
 
-        sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} service isc-dhcp-server restart
+    if [ "${tree_name}" = 'linaro' ];then
+        sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} cp -f /etc/dhcp/examples/dhcpd.conf.linaro /etc/dhcp/dhcpd.conf
+    elif [ "${tree_name}" = 'open-estuary' ];then
+        sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} cp -f /etc/dhcp/examples/dhcpd.conf.estuary /etc/dhcp/dhcpd.conf
     fi
+
+    sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} service isc-dhcp-server restart
+
 }
 
 config_dhcp "$@"
