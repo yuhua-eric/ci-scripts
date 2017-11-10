d2dec3762724d77d71ea9537046c9e2dbab04e59
qinsl0106 <qinsl0106@thundersoft.com>
2017-11-10 12:16:09 +0800

    update

---
+++ scripts/mk_tftp_dir.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# cd to tftp root dir
+root_dirs=(pxe_install/arm64 iso_install/arm64)
+versions=(estuary/v3.1 estuary/v500 linaro/16.12 estuary/current)
+
+oss=(ubuntu centos debain)
+boards=(d03 d05)
+
+for root_dir in ${root_dirs[@]};do
+    pushd .
+    mkdir -p ${root_dir}
+    cd ${root_dir}
+
+    for version in ${versions[@]};do
+        pushd .
+        mkdir -p ${version}
+        cd ${version}
+        for os in ${oss[@]};do
+            pushd .
+            mkdir -p ${os}
+            cd ${os}
+            for board in ${boards[@]};do
+                pushd .
+                mkdir -p ${board}
+                popd
+            done
+            popd
+        done
+        popd
+    done
+    popd
+done
