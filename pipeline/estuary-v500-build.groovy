f36979a1627baeb2d3c8bf5293f0ab3e9aaba0cd
qinsl0106 <qinsl0106@thundersoft.com>
2017-11-20 16:24:51 +0800

    update for parameters

--- pipeline/estuary-v500-build.groovy
+++ pipeline/estuary-v500-build.groovy
@@ -12,14 +12,6 @@ node ('ci-v500-compile'){
     stage('Preparation') { // for display purposes
         clone2local('https://github.com/qinshulei/ci-scripts.git', './local/ci-scripts')
 
-        dir('./local/ci-test-cases') {
-            deleteDir()
-        }
-        if (TEST_REPO == "" || TEST_REPO == null) {
-            TEST_REPO = "https://github.com/qinshulei/ci-test-cases.git"
-        }
-        clone2local(TEST_REPO, './local/ci-test-cases')
-
         // prepare variables.
         sh 'env'
 
@@ -31,17 +23,11 @@ node ('ci-v500-compile'){
 
         // save jenkins parameters.
         sh "echo TREE_NAME=\\\"${TREE_NAME}\\\" >> env.properties"
-        sh "echo BOOT_PLAN=\\\"${BOOT_PLAN}\\\" >> env.properties"
 
         sh "echo SHELL_PLATFORM=\\\"${SHELL_PLATFORM}\\\" >> env.properties"
         sh "echo SHELL_DISTRO=\\\"${SHELL_DISTRO}\\\" >> env.properties"
 
-        sh "echo TEST_REPO=\\\"${TEST_REPO}\\\" >> env.properties"
-        sh "echo TEST_PLAN=\\\"${TEST_PLAN}\\\" >> env.properties"
-        sh "echo TEST_LEVEL=\\\"${TEST_LEVEL}\\\" >> env.properties"
-
         sh "echo VERSION=\\\"${VERSION}\\\" >> env.properties"
-        // sh "echo GIT_DESCRIBE=\\\"${GIT_DESCRIBE}\\\" >> env.properties"
     }
 
     // load functions
