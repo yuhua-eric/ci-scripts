157285dd4551192f7321fe89f38bb580aeaa4de0
qinsl0106 <qinsl0106@thundersoft.com>
2017-11-24 20:01:48 +0800

    update

---
+++ scripts/mirror_repo_in_lava.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+MIRROR_ROOT=/root/github_mirror
+mkdir -p ${MIRROR_ROOT}
+
+cd ${MIRROR_ROOT}
+
+
+DIR_NAME=${TEST_REPO##*/}
+touch ~/.gitconfig
+
+mv ~/.gitconfig ~/.gitconfig.edit
+if [ -d "${DIR_NAME}" ];then
+    cd ${DIR_NAME}
+    git fetch
+    cd -
+else
+    git clone ${TEST_REPO} --mirror
+    echo "[url ${MIRROR_ROOT}/${DIR_NAME}/]" >> ~/.gitconfig.edit
+    echo "    insteadOf = ${TEST_REPO}" >> ~/.gitconfig.edit
+fi
+mv ~/.gitconfig.edit ~/.gitconfig
