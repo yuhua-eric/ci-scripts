1625b0ed63866cf5daf94b0887f38f014673f7c2
qinsl0106 <qinsl0106@thundersoft.com>
2017-12-20 16:15:41 +0800

    update update uefi script

--- deploy-scripts/config_uefi.sh
+++ deploy-scripts/config_uefi.sh
@@ -9,6 +9,7 @@ HOST_NAME=${2:-"d05ssh01"}
 
 cd ../
 TARGET_IP=$(python configs/parameter_parser.py -f devices.yaml -s ${HOST_NAME} -k ip)
+DEVICE_TYPE=$(python configs/parameter_parser.py -f devices.yaml -s ${HOST_NAME} -k type)
 FTP_IP=$(python configs/parameter_parser.py -f config.yaml -s Ftpinfo -k ip)
 cd -
 
@@ -21,10 +22,28 @@ function config_uefi() {
 
     if [ "${tree_name}" = 'linaro' ];then
         # do deploy
-        python update_uefi.py --uefi UEFI_D05_linaro_16_12.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+        if [ "${DEVICE_TYPE}" = "D03" ];then
+            python update_uefi.py --uefi UEFI_D03_linaro_16_12.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+        elif [ "${DEVICE_TYPE}" = "D05" ];then
+            python update_uefi.py --uefi UEFI_D05_linaro_16_12.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+        fi
+
     elif [ "${tree_name}" = 'open-estuary' ];then
-        # do deploy
-        python update_uefi.py --uefi UEFI_D05_Estuary.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+        if [ "${version_name}" = "v3.1" ];then
+            # do deploy
+            if [ "${DEVICE_TYPE}" = "D03" ];then
+                python update_uefi.py --uefi UEFI_D03_Estuary.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+            elif [ "${DEVICE_TYPE}" = "D05" ];then
+                python update_uefi.py --uefi UEFI_D05_Estuary.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+            fi
+        elif [ "${version_name}" = "v5.0" ];then
+            # do deploy
+            if [ "${DEVICE_TYPE}" = "D03" ];then
+                python update_uefi.py --uefi UEFI_D03_Estuary_V500.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+            elif [ "${DEVICE_TYPE}" = "D05" ];then
+                python update_uefi.py --uefi UEFI_D05_Estuary_V500.fd --host ${TARGET_IP} --ftp ${FTP_IP}
+            fi
+        fi
     fi
 }
 
