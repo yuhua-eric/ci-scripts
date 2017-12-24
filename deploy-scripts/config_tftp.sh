15416e0ce0cf606ed9e8dc8da41e53d1785a1517
qinsl0106 <qinsl0106@thundersoft.com>
2017-12-24 14:18:39 +0800

    add nfs change

--- deploy-scripts/config_tftp.sh
+++ deploy-scripts/config_tftp.sh
@@ -9,19 +9,38 @@ TFTP_DIR=/tftp
 TFTP_ESTUARY_GRUB=grub.cfg
 TFTP_LINARO_GRUB=linaro_install/grub.cfg
 
+
 function config_tftp() {
     local tree_name=${1:-"open-estuary"}
     local host_name=${2:-"d05ssh01"}
     local distro_name=${3:-"centos"}
     local version_name=${4:-"v3.1"}
+
+    cd ../
+    FTP_SERVER=$(python configs/parameter_parser.py -f config.yaml -s Ftpinfo -k ftpserver)
+    TARGET_IP=$(python configs/parameter_parser.py -f devices.yaml -s ${HOST_NAME} -k bmc)
+    DEVICE_TYPE=$(python configs/parameter_parser.py -f devices.yaml -s ${HOST_NAME} -k type)
+    cd -
+
     # config uefi
     if [ "${tree_name}" = 'linaro' ];then
-        cp -f /tftp/linaro_install/CentOS/linaro_centos.grub.cfg /tftp/linaro_install/grub.cfg
+        :
     elif [ "${tree_name}" = 'open-estuary' ];then
-        cp -f /tftp/estuary_install/grub.cfg /tftp/grub.cfg
+        if [ "${version_name}" != "v5.0" ];then
+            if [[ "${version_name}" =~ "estuary-" ]];then
+                if [ -d "/tftp/pxe_install/arm64/estuary/${version_name}" ];then
+                    # TODO : think diffrent distro and board
+                    cp -r "/tftp/pxe_install/arm64/estuary/v5.0" "/tftp/pxe_install/arm64/estuary/${version_name}"
+                    cd "/tftp/pxe_install/arm64/estuary/${version_name}"
+                    cd "${distro_name}"
+                    cd "${DEVICE_TYPE}"
+                    rm -rf netboot
+                    wget ${FTP_SERVER}/open-estuary/${version_name}/"${distro_name}"/netboot.tar.gz
+                    tar -xzvf netboot.tar.gz
+                fi
+            fi
+        fi
     fi
-
-    cp -f /tftp/estuary_install/estuary.txt.${distro_name,,} /tftp/estuary_install/estuary.txt
 }
 
 config_tftp "$@"
