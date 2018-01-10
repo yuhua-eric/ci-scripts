36cc866f9d8a4fd5bf13d45341c5a605d9f65821
qinsl0106 <qinsl0106@thundersoft.com>
2018-01-10 10:24:26 +0800

    wait connection

--- deploy-scripts/deploy.py
+++ deploy-scripts/deploy.py
@@ -23,6 +23,7 @@ def boot_device(BMC_HOST, BMC_USER, BMC_PASS):
     time.sleep(2)
 
     connection = shell.ipmi_connection(connection_command, 6000)
+    time.sleep(2)
     # connection.prompt_str = ['seconds to stop automatical booting']
     # connection.wait()
     # print "uefi interrupt prompt find !"
