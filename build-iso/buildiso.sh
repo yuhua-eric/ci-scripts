eafff7fd648018e27e865aae7a9be6ec05ec920e
qinsl0106 <qinsl0106@thundersoft.com>
2018-01-17 19:28:11 +0800

    update

--- build-iso/buildiso.sh
+++ build-iso/buildiso.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -ex
+# depends:
+# ubuntu
+# apt-get install genisoimage
+# apt-get install xorriso
+# centos
+# yum install genisoimage
+# wget https://www.gnu.org/software/xorriso/xorriso-1.4.8.tar.gz
+# tar -xzvf xorriso-1.4.8.tar.gz
+# ./configure && make && make install
+
+
 ./centos_mkautoiso.sh
 ./ubuntu_mkautoiso.sh
