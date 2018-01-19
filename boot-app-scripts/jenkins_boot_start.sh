f52277b012cd18695adbeb2428f98f703a5603f9
qinsl0106 <qinsl0106@thundersoft.com>
2018-01-19 17:56:51 +0800

    change padding to 5px

--- boot-app-scripts/jenkins_boot_start.sh
+++ boot-app-scripts/jenkins_boot_start.sh
@@ -460,7 +460,7 @@ EOF
     cd ${WORKSPACE}/local/ci-scripts/boot-app-scripts/${GIT_DESCRIBE}/${RESULTS_DIR}
     echo  ""
     echo "Test summary is below:<br>" >> ${WORKSPACE}/MAIL_CONTENT.txt
-    echo '<table cellspacing="0" cellpadding="15px" border="1">' >> ${WORKSPACE}/MAIL_CONTENT.txt
+    echo '<table cellspacing="0" cellpadding="5px" border="1">' >> ${WORKSPACE}/MAIL_CONTENT.txt
     echo '<tr style="text-align: center;justify-content: center;background-color: #b9bbc0;"><th>Type</th><th>Total_Number</th><th>Failed_Number</th><th>Success_Number</th></tr>' >> ${WORKSPACE}/MAIL_CONTENT.txt
     cat whole_summary.txt |
         awk -F" " '{print "<tr style=\"text-align: center;justify-content: center;\">" "<td>" $1 "</td><td>" $2 "</td><td>" $3 "</td><td>" $4 "</td></tr>"}' >> ${WORKSPACE}/MAIL_CONTENT.txt
@@ -470,7 +470,7 @@ EOF
 
     echo  ""
     echo "The Test Case details is below:<br>" >> ${WORKSPACE}/MAIL_CONTENT.txt
-    echo '<table cellspacing="0" cellpadding="15px" border="1">' >> ${WORKSPACE}/MAIL_CONTENT.txt
+    echo '<table cellspacing="0" cellpadding="5px" border="1">' >> ${WORKSPACE}/MAIL_CONTENT.txt
     echo '<tr style="text-align: center;justify-content: center;background-color: #b9bbc0;"><th>Job_ID</th><th>Suite_Name</th><th>Case_Name</th><th>Case_Result</th></tr>' >> ${WORKSPACE}/MAIL_CONTENT.txt
     cat details_summary.txt |
         awk -F" " '{print "<tr style=\"text-align: center;justify-content: center;\">" "<td><a href=\"" "'"${LAVA_DISPLAY_URL}/results/"'" $1 "\">" $1 "</a><td>" $2 "</td><td>" $3 "</td><td>" $4 "</td></tr>"}' >> ${WORKSPACE}/MAIL_CONTENT.txt
