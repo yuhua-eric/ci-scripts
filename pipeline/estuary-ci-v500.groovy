2ac88e535d98857dbf35e7f62c0ea486c7347627
qinsl0106 <qinsl0106@thundersoft.com>
2018-01-24 20:04:15 +0800

    update

---
+++ pipeline/estuary-ci-v500.groovy
@@ -0,0 +1,133 @@
+def clone2local(giturl, localdir) {
+    def exists = fileExists localdir
+    if (!exists){
+        new File(localdir).mkdir()
+    }
+    dir (localdir) {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+                extensions: [[$class: 'CloneOption', timeout: 120]], gitTool: 'Default',
+                userRemoteConfigs: [[url: giturl]]
+            ])
+    }
+}
+
+node ('ci-v500-compile'){
+    stage('Build Preparation') { // for display purposes
+        clone2local('https://github.com/qinshulei/ci-scripts.git', './local/ci-scripts')
+
+        // prepare variables.
+        sh 'env'
+
+        // save the properties
+        sh 'echo "" > env.properties'
+
+        // save jenkins enviroment properties.
+        sh "echo BUILD_URL=\\\"${BUILD_URL}\\\" >> env.properties"
+
+        // save jenkins parameters.
+        sh "echo TREE_NAME=\\\"${TREE_NAME}\\\" >> env.properties"
+        sh "echo BOOT_PLAN=\\\"${BOOT_PLAN}\\\" >> env.properties"
+
+        sh "echo SHELL_PLATFORM=\\\"${SHELL_PLATFORM}\\\" >> env.properties"
+        sh "echo SHELL_DISTRO=\\\"${SHELL_DISTRO}\\\" >> env.properties"
+
+        sh "echo TEST_REPO=\\\"${TEST_REPO}\\\" >> env.properties"
+        sh "echo TEST_PLAN=\\\"${TEST_PLAN}\\\" >> env.properties"
+        sh "echo TEST_LEVEL=\\\"${TEST_LEVEL}\\\" >> env.properties"
+
+        sh "echo SUCCESS_MAIL_LIST=\\\"${SUCCESS_MAIL_LIST}\\\" >> env.properties"
+        sh "echo FAILED_MAIL_LIST=\\\"${FAILED_MAIL_LIST}\\\" >> env.properties"
+    }
+    // load functions
+    def functions = load "./local/ci-scripts/pipeline/functions.groovy"
+
+    def build_result = 0
+    stage('Build') {
+        build_result = sh script: "./local/ci-scripts/build-scripts/jenkins_build_v500_start.sh -p env.properties 2>&1 ", returnStatus: true
+    }
+    echo "build_result : ${build_result}"
+    if (build_result == 0) {
+        echo "build success"
+    } else {
+        echo "build failed"
+        functions.send_mail()
+        currentBuild.result = 'FAILURE'
+        return
+    }
+
+    def iso_result = 0
+    dir('./local/ci-scripts/build-iso') {
+        iso_result = sh script: "./buildiso.sh 2>&1 ", returnStatus: true
+    }
+    echo "iso_result : ${iso_result}"
+    if (iso_result == 0) {
+        echo "iso success"
+    } else {
+        echo "iso failed"
+        functions.send_mail()
+        currentBuild.result = 'FAILURE'
+        return
+    }
+
+    stage('stash') {
+        // stash result
+        dir('/fileserver/open-estuary') {
+            stash includes: '**/*', name: 'buildResult'
+        }
+
+        stash includes: '*.txt', name: 'mailResult'
+        stash includes: '*.properties', name: 'paramsResult'
+    }
+}
+
+node('ci-compile') {
+    stage('upload Binary') {
+        // unstash result
+        dir('/fileserver/open-estuary'){
+            unstash 'buildResult'
+        }
+    }
+
+    stage('Result') {
+        unstash 'mailResult'
+        unstash 'paramsResult'
+    }
+
+    clone2local('https://github.com/qinshulei/ci-scripts.git', './local/ci-scripts')
+    // load functions
+    def functions = load "./local/ci-scripts/pipeline/functions.groovy"
+
+    stage('Test Preparation') { // for display purposes
+        clone2local('https://github.com/qinshulei/ci-scripts.git', './local/ci-scripts')
+
+        dir('./local/ci-test-cases') {
+            deleteDir()
+        }
+        if (TEST_REPO == "" || TEST_REPO == null) {
+            TEST_REPO = "https://github.com/qinshulei/ci-test-cases.git"
+        }
+        clone2local(TEST_REPO, './local/ci-test-cases')
+    }
+
+    stage ('mirror') {
+        build job: 'step_mirror_test_repo_in_lava', parameters: [[$class: 'StringParameterValue', name: 'TEST_REPO', value: TEST_REPO]]
+    }
+
+    def test_result = 0
+    stage('Test') {
+        test_result = sh script: "./local/ci-scripts/boot-app-scripts/jenkins_boot_start.sh -p env.properties 2>&1 " , returnStatus: true
+    }
+    echo "test_result : ${test_result}"
+    if (test_result == 0) {
+        echo "Test success"
+    } else {
+        echo "Test failed"
+        functions.send_mail()
+        currentBuild.result = 'FAILURE'
+        return
+    }
+
+    stage('Result') {
+        functions.send_mail()
+    }
+}
