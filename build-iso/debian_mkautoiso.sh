7329358624555a8981275ef5b99dd44addbc0838
qinsl0106 <qinsl0106@thundersoft.com>
2018-02-02 16:06:06 +0800

    fix path

--- build-iso/debian_mkautoiso.sh
+++ build-iso/debian_mkautoiso.sh
@@ -32,7 +32,7 @@ mount $material_iso ./mnt
 cp -rf ./mnt/* ./mnt/.disk/ ./debian/
 
 cp $cfg_path$new_grub ./debian/boot/grub/
-cp $cfg_path$new_preseed ./debian/preseed/
+cp $cfg_path$new_preseed ./debian/
 
 
 xorriso -as mkisofs -r -checksum_algorithm_iso md5,sha1 -o ./$new_iso -J -joliet-long -cache-inodes -e boot/grub/efi.img -no-emul-boot -append_partition 2 0xef debian/boot/grub/efi.img -partition_cyl_align all debian/
