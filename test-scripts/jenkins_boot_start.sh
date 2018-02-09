abf17815b26c1c0762457cd36fe61c4057eb2359
qinsl0106 <qinsl0106@thundersoft.com>
2018-02-09 22:36:18 +0800

    fix workaround stash devices

--- test-scripts/jenkins_boot_start.sh
+++ test-scripts/jenkins_boot_start.sh
@@ -497,8 +497,8 @@ function workaround_stash_devices_config() {
     else
         CI_ENV=dev
     fi
-    if [ -e "configs/"${CI_ENV}"/devices.yaml" ];then
-        cp -f configs/"${CI_ENV}"/devices.yaml /tmp/devices.yaml
+    if [ -e "${CI_SCRIPTS_DIR}/configs/${CI_ENV}/devices.yaml" ];then
+        cp -f "${CI_SCRIPTS_DIR}/configs/${CI_ENV}/devices.yaml" /tmp/devices.yaml
     fi
 }
 
@@ -510,7 +510,7 @@ function workaround_pop_devices_config() {
     fi
 
     if [ -e "/tmp/devices.yaml" ];then
-        cp -f /tmp/devices.yaml configs/"${CI_ENV}"/devices.yaml
+        cp -f /tmp/devices.yaml "${CI_SCRIPTS_DIR}/configs/${CI_ENV}/devices.yaml"
     fi
 }
 
