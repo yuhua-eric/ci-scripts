93842bfde0010973ac21b3913b3e4c00feeaf614
qinsl0106 <qinsl0106@thundersoft.com>
2018-02-24 10:53:46 +0800

    update

--- test-scripts/html-table.py
+++ test-scripts/html-table.py
@@ -2,11 +2,15 @@
 # -*- coding=utf-8 -*-
 #
 # Author by : qinsl0106@thundersoft.com
+# generate html table
 
 import json
 import sys
 import os
 import argparse
+
+style = 'style="border: solid 1px black;"'
+
 
 def load_json(file_name):
     with open(file_name) as f:
@@ -47,34 +51,34 @@ def get_rows(rows, row_span_arr, row_str):
             for item in rows:
                 # has dict element, which means has link or color
                 if isinstance(item, dict):
-                    if item.has_key('link') and item.has_key('color'):
-                        row_str += ('<td style="border: solid 1px black;"> <a href="' + item['link'] + '" style="color:' +item['color']+ '">' +
-                            item['data'] + '</a></td>\n')
-                    elif item.has_key('link'):
-                        row_str += ('<td style="border: solid 1px black;"> <a href="' + item[
-                            'link'] + '">' + item['data'] + '</a></td>\n')
-                    elif item.has_key('color'):
-                        row_str += ('<td style="border: solid 1px black;"> <font color="' + item[
-                            'color'] + '">' + item['data'] + '</font></td>\n')
+                    row_str += extract_dict(item)
                 else:
-                    row_str += ('<td style="border: solid 1px black;">' + str(
+                    row_str += ('<td ' + style + '>' + str(
                         item) + '</td>\n')
             row_str += '</tr>\n<tr>\n'
         else:
             rowspan = row_span_arr.pop(0)
             if isinstance(rows, dict):
-                if rows.has_key('link') and rows.has_key('color'):
-                    row_str += '\n<td rowspan=%s style="border: solid 1px black;">' % rowspan + '<a href="' + rows['link'] + '" style="color:' +rows['color']+ '">' +rows['data'] + '</a></td>\n'
-                elif rows.has_key('link'):
-                    row_str += ('<td rowspan=%s style="border: solid 1px black;"> <a href="'%rowspan + rows[
-                        'link'] + '">' + rows['data'] + '</a></td>\n')
-                elif rows.has_key('color'):
-                    row_str += ('<td rowspan=%s style="border: solid 1px black;"> <font color="'%rowspan + rows[
-                        'color'] + '">' + rows['data'] + '</font></td>\n')
+                row_str += extract_dict(rows, rowspan)
             else:
-                row_str += '\n<td rowspan=%s style="border: solid 1px black;">' % rowspan + str(
+                row_str += '\n<td rowspan=%s '% rowspan + style + '>' + str(
                     rows) + '</td>\n'
 
+    return row_str
+
+
+def extract_dict(data, rowspan=1):
+    row_str = ''
+    if data.has_key('link') and data.has_key('color'):
+        row_str += ('<td rowspan=%s '%rowspan + style + '> <a href="' + data['link'] + '" style="color:' + data[
+            'color'] + '">' +
+                    data['data'] + '</a></td>\n')
+    elif data.has_key('link'):
+        row_str += ('<td rowspan=%s '%rowspan + style + '>  <a href="' + data[
+            'link'] + '">' + data['data'] + '</a></td>\n')
+    elif data.has_key('color'):
+        row_str += ('<td rowspan=%s '%rowspan + style + '>  <font color="' + data[
+            'color'] + '">' + data['data'] + '</font></td>\n')
     return row_str
 
 
@@ -96,23 +100,22 @@ def get_col(column):
             if index in tmp:
                 continue
             if index in tmp2:
-                col_str += '<td colspan=%s style="border: solid 1px black;">%s</td>\n' % (
-                    len(column[index + 1]), column[index])
+                col_str += '<td colspan=%s '%len(column[index + 1]) + style + '>%s</td>\n' % (
+                   column[index])
             else:
-                col_str += '<td rowspan=2 style="border: solid 1px black;"> %s</td>\n' % \
-                           column[index]
+                col_str += '<td rowspan=2 ' + style + '> %s</td>\n' % column[index]
 
         col_str += '</tr>\n<tr>'
         for index in tmp:
             for item in column[index]:
-                col_str += '<td style="border: solid 1px black;">%s</td>\n' % item
+                col_str += '<td '+ style + '>%s</td>\n' % item
         col_str += '</tr>'
         return col_str
     else:
         col_str = ''
         col_str += '<tr style="text-align: center;justify-content: center">\n'
         for item in column:
-            col_str += '<td style="border: solid 1px black;">%s</td>\n' % item
+            col_str += '<td ' + style + '>%s</td>\n' % item
         col_str += '</tr>'
         return col_str
 
@@ -135,8 +138,12 @@ def main():
 
     content = "<tr>\n" + get_rows(rows_temp, row_span_arr, '')[:-5]
     column = get_col(column_temp)
-    print '<table cellspacing="0px" style="border: solid 1px black;">\n'+ column + '\n' + content + '</table>'
+    print '<table cellspacing="0px" '+ style + '>\n'+ column + '\n' + content + '</table>'
 
+    # for test use
+    # x = '<table cellspacing="0px" '+ style + '>\n'+ column + '\n' + content + '</table>'
+    # with open('test.txt', 'w') as f:
+    #     f.write(x)
 
 if __name__ == '__main__':
     main()
