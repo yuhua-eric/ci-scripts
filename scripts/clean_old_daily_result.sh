6fe00ac952687856d669b4edd1617b67000992e7
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-21 12:29:20 +0800

    add clean daily result script

---
+++ scripts/clean_old_daily_result.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+function clean_last_n_day_dir() {
+    local target_dir=$1
+    local n_day=$2
+    cd ${target_dir}
+    DEL_DATE=$(date '+%Y%m%d' --date="-${n_day} day")
+    echo ${DEL_DATE}
+    rm -rf ./*${DEL_DATE}*
+    cd -
+}
+
+clean_last_n_day_dir "/fileserver/open-estuary" 7
