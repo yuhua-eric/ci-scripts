574ceaece4041627cc3c3f3b6c57c7910fc64aab
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-23 14:46:59 +0800

    fix count issue

--- test-scripts/estuary-report.py
+++ test-scripts/estuary-report.py
@@ -929,32 +929,26 @@ def generate_module_dict(result_json_dict, test_dir):
             name_dict[key][sub_key]["pass"] = 0
             name_dict[key][sub_key]["fail"] = 0
 
-    for job_key in result_json_dict:
-        result = result_json_dict[job_key]
-        # print owner
-        for item in result:
-            if item.has_key('suite') and item['suite'] != 'lava':
-                suit_name = item['suite'][2:]
-                for key in name_dict.keys():
-                    for sub_key in name_dict[key].keys():
-                        if check_special_module_dict_key(sub_key):
-                            continue
-                        for suite_key in name_dict[key][sub_key].keys():
-                            if check_special_module_dict_key(suite_key):
-                                continue
-                            for case_key in name_dict[key][sub_key][suite_key].keys():
-                                if name_dict[key][sub_key][suite_key][case_key] == "pass":
-                                    name_dict[key][sub_key]["pass"] += 1
-                                    name_dict[key]["pass"] += 1
-                                    name_dict[key][sub_key]["total"] += 1
-                                    name_dict[key]["total"] += 1
-                                elif name_dict[key][sub_key][suite_key][case_key] == "fail":
-                                    name_dict[key][sub_key]["fail"] += 1
-                                    name_dict[key]["fail"] += 1
-                                    name_dict[key][sub_key]["total"] += 1
-                                    name_dict[key]["total"] += 1
-                                else:
-                                    print "WARNING: the result not pass or fail" + name_dict[key][sub_key][suite_key][case_key]
+    for key in name_dict.keys():
+        for sub_key in name_dict[key].keys():
+            if check_special_module_dict_key(sub_key):
+                continue
+            for suite_key in name_dict[key][sub_key].keys():
+                if check_special_module_dict_key(suite_key):
+                    continue
+                for case_key in name_dict[key][sub_key][suite_key].keys():
+                    if name_dict[key][sub_key][suite_key][case_key] == "pass":
+                        name_dict[key][sub_key]["pass"] += 1
+                        name_dict[key]["pass"] += 1
+                        name_dict[key][sub_key]["total"] += 1
+                        name_dict[key]["total"] += 1
+                    elif name_dict[key][sub_key][suite_key][case_key] == "fail":
+                        name_dict[key][sub_key]["fail"] += 1
+                        name_dict[key]["fail"] += 1
+                        name_dict[key][sub_key]["total"] += 1
+                        name_dict[key]["total"] += 1
+                    else:
+                        print "WARNING: the result not pass or fail" + name_dict[key][sub_key][suite_key][case_key]
     return name_dict
 
 def print_scope_result(name_dict, jenkins_build_url, distro):
