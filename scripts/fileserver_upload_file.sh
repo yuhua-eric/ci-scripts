c97b3ca2cdef69c8d22fbcb686d95facb1ef377c
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-27 18:20:45 +0800

    update

--- scripts/fileserver_upload_file.sh
+++ scripts/fileserver_upload_file.sh
@@ -19,11 +19,11 @@ cp ${WORKSPACE}/data.tar.gz ./${FILENAME}
 echo "http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}"
 echo "http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}"
 
-mkdir -p ${WORKSPACE}/html/
-cat > ${WORKSPACE}/html/index.html <<-EOF
-<p>文件已经上传到该路径 <a href=\"http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}</a> </p>
-<p>你可以通过该连接在CI内网访问 <a href=\"http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}</a></p>
-<p>你可以通过以下命令在命令行下载该文件:</p>
+mkdir -p ${WORKSPACE}/html_result/
+cat > ${WORKSPACE}/html_result/index.html <<-EOF
+<p>File have been upload: <a href=\"http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}</a> </p>
+<p>You can access this file by this link in CI enviroment: <a href=\"http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}</a></p>
+<p>Download Commandline:</p>
 <pre>
   wget http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}
 </pre>
