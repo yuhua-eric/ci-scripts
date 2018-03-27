20be37854e7406662e0d1c9bfb444b00cfd92a44
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-27 18:24:57 +0800

    update

--- scripts/fileserver_upload_file.sh
+++ scripts/fileserver_upload_file.sh
@@ -21,8 +21,8 @@ echo "http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}"
 
 mkdir -p ${WORKSPACE}/html_result/
 cat > ${WORKSPACE}/html_result/index.html <<-EOF
-<p>File have been upload: <a href=\"http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}</a> </p>
-<p>You can access this file by this link in CI enviroment: <a href=\"http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}\">http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}</a></p>
+<p>File have been upload: <a href="http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}">http://120.31.149.194:18083/upload_data/${TODAY}/${UUID}/${FILENAME}</a> </p>
+<p>You can access this file by this link in CI enviroment: <a href="http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}">http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}</a></p>
 <p>Download Commandline:</p>
 <pre>
   wget http://192.168.50.122:8083/upload_data/${TODAY}/${UUID}/${FILENAME}
