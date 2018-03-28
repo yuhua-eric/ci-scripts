8fb7b1ae7da5715b277b9328c973bd012f9470b6
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-28 18:19:26 +0800

    fix

--- scripts/refresh_dhcp.sh
+++ scripts/refresh_dhcp.sh
@@ -3,8 +3,6 @@
 __ORIGIN_PATH__="$PWD"
 script_path="${0%/*}"  # remove the script name ,get the path
 script_path=${script_path/\./$(pwd)} # if path start with . , replace with $PWD
-source "${script_path}/../common.sh"
-
 cd "${script_path}"
 
 # read config from config file
@@ -17,6 +15,5 @@ DHCP_CONFIG_DIR=/etc/dhcp
 DHCP_FILENAME=dhcpd.conf
 
 ./scripts/gen_dhcpd_conf.sh > dhcpd.conf;
-
-sshpass -p 'root' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ../dhcpd.conf root@${DHCP_SERVER}:/etc/dhcp/dhcpd.conf
+sshpass -p 'root' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null dhcpd.conf root@${DHCP_SERVER}:/etc/dhcp/dhcpd.conf
 sshpass -p 'root' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${DHCP_SERVER} service isc-dhcp-server restart
