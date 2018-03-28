c3c40a33efc17330a2638647441f019eb99d2a35
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-28 18:18:07 +0800

    update

--- scripts/refresh_dhcp.sh
+++ scripts/refresh_dhcp.sh
@@ -3,7 +3,7 @@
 __ORIGIN_PATH__="$PWD"
 script_path="${0%/*}"  # remove the script name ,get the path
 script_path=${script_path/\./$(pwd)} # if path start with . , replace with $PWD
-source "${script_path}/common.sh"
+source "${script_path}/../common.sh"
 
 cd "${script_path}"
 
