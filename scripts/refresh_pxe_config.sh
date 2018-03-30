d10f17b69ab4854baef7ae3c46dae1f251ffa9e2
qinsl0106 <qinsl0106@thundersoft.com>
2018-03-30 11:55:32 +0800

    update

--- scripts/refresh_pxe_config.sh
+++ scripts/refresh_pxe_config.sh
@@ -3,16 +3,16 @@
 # backup old cfg
 # centos
 cp ./configs/auto-install/centos/auto-pxe/anaconda-ks.cfg /fileserver/estuary_v500/CentOS/mirror/anaconda-ks.cfg
-# cp ./configs/auto-install/centos/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/centos/d03/grub.cfg
-# cp ./configs/auto-install/centos/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/centos/d05/grub.cfg
+cp ./configs/auto-install/centos/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/centos/d03/grub.cfg
+cp ./configs/auto-install/centos/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/centos/d05/grub.cfg
 
 # debian
-# cp ./configs/auto-install/debian/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/debian/d03/grub.cfg
-# cp ./configs/auto-install/debian/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/debian/d05/grub.cfg
+cp ./configs/auto-install/debian/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/debian/d03/grub.cfg
+cp ./configs/auto-install/debian/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/debian/d05/grub.cfg
 cp ./configs/auto-install/debian/auto-pxe/preseed.cfg /fileserver/estuary_v500/Debian/preseed.cfg
 
 # ubuntu
-# cp ./configs/auto-install/ubuntu/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/ubuntu/d03/grub.cfg
-# cp ./configs/auto-install/ubuntu/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/ubuntu/d05/grub.cfg
+cp ./configs/auto-install/ubuntu/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/ubuntu/d03/grub.cfg
+cp ./configs/auto-install/ubuntu/auto-pxe/grub.cfg /tftp/pxe_install/arm64/estuary/template/ubuntu/d05/grub.cfg
 cp ./configs/auto-install/ubuntu/auto-pxe/ubuntu_ks.cfg /fileserver/estuary_v500/Ubuntu/mirror/ubuntu_ks.cfg
 cp ./configs/auto-install/ubuntu/auto-pxe/ubuntu.seed /fileserver/estuary_v500/Ubuntu/mirror/preseed/ubuntu.seed
