0e79dc42d4013d4f47d4a24a80eb98f1a9bdc1b1
yuhua-eric <38514455+yuhua-eric@users.noreply.github.com>
2018-05-22 18:24:33 +0800

    Update buildiso.sh

--- build-iso-scripts/buildiso.sh
+++ build-iso-scripts/buildiso.sh
@@ -79,7 +79,7 @@ function main() {
     ./fedora_mkautoiso.sh "${GIT_DESCRIBE}"
     #./opensuse_mkautoiso.sh "${GIT_DESCRIBE}"
     cp_opensuse_iso
-    docker run --privileged=true -it  -v /home:/root/ --name opensuse estuary/opensuse:5.1-full bash /root/jenkins/workspace/estuary-v500-build/local/ci-scripts/build-iso-scripts/opensuse_mkautoiso.sh
+    docker run --privileged=true -i -v /home:/root/ --name opensuse estuary/opensuse:5.1-full bash /root/jenkins/workspace/estuary-v500-build/local/ci-scripts/build-iso-scripts/opensuse_mkautoiso.sh
     cp_auto_iso
 }
 
