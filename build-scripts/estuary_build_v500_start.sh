d49fa8e0f2f1ec145b55d60191a3f31f7dd31c97
yuhua-eric <you@example.clsom>
2018-08-07 17:45:50 +0800

    put common to build first

--- build-scripts/estuary_build_v500_start.sh
+++ build-scripts/estuary_build_v500_start.sh
@@ -219,11 +219,12 @@ function do_build() {
         # Execute build
         pushd estuary
         # TODO : workaround for build all in single machine
+        ./build.sh --build_dir=${BUILD_DIR} -d common &
+	sleep 1m
         for DISTRO in $ALL_SHELL_DISTRO;do
             ./build.sh --build_dir=${BUILD_DIR} -d "${DISTRO,,}" &
             sleep 1m
         done
-        ./build.sh --build_dir=${BUILD_DIR} -d common &
         wait
 
         # ./build.sh --build_dir=${BUILD_DIR}
