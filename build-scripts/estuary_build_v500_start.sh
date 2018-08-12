71a97894e47d80fe5b53dcfd63add94b6f9cb96b
yuhua-eric <15099996763@163.com>
2018-08-12 19:06:36 +0800

    add info

--- build-scripts/estuary_build_v500_start.sh
+++ build-scripts/estuary_build_v500_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
-#: Title                  : jenkins_build_v500_start.sh
-#: Usage                  : ./local/ci-scripts/build-scripts/jenkins_build_v500_start.sh -p env.properties
-#: Author                 : qinsl0106@thundersoft.com
-#: Description            : CI中自动编译的jenkins任务脚本，针对v500
+#: Title                  : esturay_build_v500_start.sh
+#: Usage                  : ./local/ci-scripts/build-scripts/estuary_build_v500_start.sh -p env.properties
+#: Author                 : yu_hua1@hoperun.com
+#: Description            : CI编译工程专用脚本（estuary-v500-build），针对v500
 # only works on centos
 
 __ORIGIN_PATH__="$PWD"
