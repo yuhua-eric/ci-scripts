4a46d1dbc7265281d6edb82b61f91587e076912c
yuhua-eric <you@example.clsom>
2018-08-16 11:29:01 +0800

    modi do_build

--- build-scripts/estuary_build_v500_start.sh
+++ build-scripts/estuary_build_v500_start.sh
@@ -232,12 +232,11 @@ function do_build() {
         # Execute build
         pushd estuary
         # TODO : workaround for build all in single machine
-        ./build.sh --build_dir=${BUILD_DIR} -d common &
-	sleep 1m
-        for DISTRO in $ALL_SHELL_DISTRO;do
-            ./build.sh --build_dir=${BUILD_DIR} -d "${DISTRO,,}" &
-            sleep 1m
+	 for DISTRO in $ALL_SHELL_DISTRO;do
+            ./build.sh --build_dir=${BUILD_DIR} -d "${DISTRO,,}" |tee > ${DISTRO}.log 2>&1 &
+            #sleep 1m
         done
+        ./build.sh --build_dir=${BUILD_DIR} -d common |tee > common.log 2>&1 &
         wait
 
         # ./build.sh --build_dir=${BUILD_DIR}
