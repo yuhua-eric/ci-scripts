bcbd3f97f8ce50b43680cd3ad4ee6ac93ff03a34
yuhua-eric <you@example.clsom>
2018-08-20 09:28:26 +0800

    change build node

--- pipeline/estuary-ci-v500.groovy
+++ pipeline/estuary-ci-v500.groovy
@@ -19,7 +19,7 @@ def getGitUrl() {
     return scm.getUserRemoteConfigs()[0].getUrl()
 }
 
-node ('ci-v500-compile'){
+node ('ci-v500-compile_1'){
 
 
     stage('Build Preparation') { // for display purposes
