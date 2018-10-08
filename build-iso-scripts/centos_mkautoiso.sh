ce13bd08e8ddf1161fbf828353c4255b2f9bd4dd
lin755 <billkid007@icloud.com>
2018-10-08 16:34:07 +0800

    Update centos_mkautoiso.sh

--- build-iso-scripts/centos_mkautoiso.sh
+++ build-iso-scripts/centos_mkautoiso.sh
@@ -17,7 +17,7 @@ if [ -z ${VERSION} ];then
     exit 1
 fi
 
-material_iso=$(ls /home/fileserver/open-estuary/${VERSION}/CentOS/*CentOS*.iso)
+material_iso=$(ls /home/fileserver/open-estuary/${VERSION}/CentOS/*everything*.iso)
 if [ -z "${material_iso}" ];then
     exit 1
 fi
