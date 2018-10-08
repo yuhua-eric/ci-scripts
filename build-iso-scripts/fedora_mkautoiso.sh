f8f4a1983b2399997b4b748a19a22ad44b1cc36a
lin755 <billkid007@icloud.com>
2018-10-08 16:35:32 +0800

    Update fedora_mkautoiso.sh

--- build-iso-scripts/fedora_mkautoiso.sh
+++ build-iso-scripts/fedora_mkautoiso.sh
@@ -17,7 +17,7 @@ if [ -z ${VERSION} ];then
     exit 1
 fi
 
-material_iso=$(ls /home/fileserver/open-estuary/${VERSION}/Fedora/*Fedora*.iso)
+material_iso=$(ls /home/fileserver/open-estuary/${VERSION}/Fedora/*everything*.iso)
 if [ -z "${material_iso}" ];then
     exit 1
 fi
